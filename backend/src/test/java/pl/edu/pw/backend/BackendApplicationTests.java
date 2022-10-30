d6ab17fcc10698ab20f842bfa5e5165c03bd67c0
KomorowskiKuba <komorii2k@gmail.com>
2022-10-30 19:52:08 +0100

    Added basic containerized spring application w postgres db

---
+++ backend/src/test/java/pl/edu/pw/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
