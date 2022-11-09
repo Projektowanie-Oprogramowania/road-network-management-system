869ecfc888fc7e565c0c841f6f2ec6ee0ce7f137
hubertnakielski <01153087@pw.edu.pl>
2022-11-09 22:15:40 +0100

    database config

--- backend/src/main/java/pl/edu/pw/backend/infrastructure/InfrastructureDTO.java
+++ backend/src/main/java/pl/edu/pw/backend/infrastructure/InfrastructureDTO.java
@@ -2,10 +2,12 @@ package pl.edu.pw.backend.infrastructure;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import pl.edu.pw.backend.point.Point;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 class InfrastructureDTO {
 
     private String name;
