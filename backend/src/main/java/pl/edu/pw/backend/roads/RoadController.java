a232da157103c221985a7cd8c86b0f3919c1f900
Hubert Nakielski <hubert.nakielski@tink.com>
2022-11-30 21:48:44 +0100

    add all roads endpoint

--- backend/src/main/java/pl/edu/pw/backend/roads/RoadController.java
+++ backend/src/main/java/pl/edu/pw/backend/roads/RoadController.java
@@ -1,12 +1,17 @@
 package pl.edu.pw.backend.roads;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
@@ -38,6 +43,11 @@ public class RoadController {
     @GetMapping("/region/{id}")
     List<RoadDTO> getRoadByRegion(@PathVariable int id) {
         return roadService.getRoadsByRegion(id);
+    }
+
+    @GetMapping
+    List<RoadDTO> getRoads() {
+        return roadService.getRoads();
     }
 
 }
